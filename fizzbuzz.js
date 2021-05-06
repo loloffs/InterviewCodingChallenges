916e30584004ee59bd7cfe416b9ee7e72d083d12
loloffs <lukeoloffs@gmail.com>
2021-05-06 15:08:01 -0700

    fizzbuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,3 +1,14 @@
-const fizzbuzz = function() {
-  
+const fizzbuzz = function(range, multiples, words) {
+  for (let i = range[0]; i <= range[1]; i++)
+  if ((i % multiples[0] === 0) && (i % multiples[1] === 0)) {
+    console.log(words[0] + words[1]);
+  } else if (i % multiples[0] === 0) {
+    console.log(words[0])
+  } else if (i % multiples[1] === 0) {
+    console.log(words[1]);
+  } else {
+    console.log(i);
+  }
 }
+
+fizzbuzz([15, 90], [2, 5], ["Batty", "Beacon"]);
